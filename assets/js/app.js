a5d076d17883c3ffae89ee36bba4dcac17f31d8c
psong1 <paulsong29@gmail.com>
2023-02-05 15:44:10 -0500

    initial framework

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+$(document).foundation()
+
+let haloKey = '576858f4b6fa477990afac4add5f794f';
+
+const getGameInfo = () => {
+    let getGameUrl ='https://api.rawg.io/api/games/halo-combat-evolved';
+    
+
+}
+
