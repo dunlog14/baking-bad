8355c1f9fae425d439dfc54b62c5dea173c7e15c
psong1 <paulsong29@gmail.com>
2023-02-05 22:20:06 -0500

    made cards for games and added audio for easter egg

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,9 +2,27 @@ $(document).foundation()
 
 let haloKey = '576858f4b6fa477990afac4add5f794f';
 
+
 const getGameInfo = () => {
     let getGameUrl ='https://api.rawg.io/api/games/halo-combat-evolved';
     
 
 }
+
+// function to play theme song (EASTER EGG)
+function PlaySound(soundobj) {
+    var thissound=document.getElementById('mySound');
+    thissound.play();
+    thissound.volume = 0.2;
+// thissound.addEventListener('mouseover', PlaySound());
+}
+
+function StopSound(soundobj) {
+    var thissound=document.getElementById('mySound');
+    thissound.pause();
+    thissound.currentTime = 0;
+}
+
+
+
 
