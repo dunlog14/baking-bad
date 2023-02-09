615cdf5d11865f82d51b0aaa0d4a78bf0c931474
psong1 <paulsong29@gmail.com>
2023-02-08 20:25:16 -0500

    added modals and triggers w/ jquery

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,29 +1,40 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, options);
+$(document).ready(function(){
+    $('.modal').modal();
   });
 
 let rawgKey = '576858f4b6fa477990afac4add5f794f';
 
-function halo1() {
-async function fetchData() {
-    return (await fetch('https://api.rawg.io/api/games/halo-combat-evolved?key=576858f4b6fa477990afac4add5f794f')).json();
-}
-document.addEventListener("DOMContentLoaded", async () => {
-    let games = [];
-    try {
-        games = await fetchData();
-    } catch (e) {
-        // console.log("Error!");
-        // console.log(e);
-    }
-    console.log(games);
+// function halo1() {
+// async function fetchData() {
+//     return (await fetch('https://api.rawg.io/api/games/halo-combat-evolved?key=576858f4b6fa477990afac4add5f794f')).json();
+// }
+// document.addEventListener("DOMContentLoaded", async () => {
+//     let games = [];
+//     try {
+//         games = await fetchData();
+//     } catch (e) {
+//         // console.log("Error!");
+//         // console.log(e);
+//     }
+//     console.log(games);
+// });
+// };
+
+// halo1();
+
+function getApi() {
+
+    var requestUrl = 'https://api.rawg.io/api/games/halo-combat-evolved?key=576858f4b6fa477990afac4add5f794f';
+  
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
 });
-};
-
-halo1();
-
-
+}     
+    getApi();
   
 // function to play theme song (EASTER EGG)
 function PlaySound(soundobj) {
