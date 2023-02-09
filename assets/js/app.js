924af6ef932cc9bfca8d6c3d925f98e99cab6599
psong1 <paulsong29@gmail.com>
2023-02-08 22:32:41 -0500

    finally pushed data to modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,25 +4,7 @@ $(document).ready(function(){
 
 let rawgKey = '576858f4b6fa477990afac4add5f794f';
 
-// function halo1() {
-// async function fetchData() {
-//     return (await fetch('https://api.rawg.io/api/games/halo-combat-evolved?key=576858f4b6fa477990afac4add5f794f')).json();
-// }
-// document.addEventListener("DOMContentLoaded", async () => {
-//     let games = [];
-//     try {
-//         games = await fetchData();
-//     } catch (e) {
-//         // console.log("Error!");
-//         // console.log(e);
-//     }
-//     console.log(games);
-// });
-// };
-
-// halo1();
-
-function getApi() {
+function halo1() {
 
     var requestUrl = 'https://api.rawg.io/api/games/halo-combat-evolved?key=576858f4b6fa477990afac4add5f794f';
   
@@ -31,11 +13,122 @@ function getApi() {
         return response.json();
       })
       .then(function (data) {
-        console.log(data);
-});
-}     
-    getApi();
+        // console.log(data);
+        console.log(data.name);
+        console.log(data.metacritic);
+        console.log(data.description);
+        console.log(data.released);
+
+    let oneHead = $('.one');
+    let oneDescription = $('.game-description-1');
+ 
+    let oneTitle = data.name;
+        oneHead.append(oneTitle);
+    let oneAbout = data.description;
+        oneDescription.append(oneAbout);
+
   
+        
+});
+}
+
+
+  function halo2() {
+
+    var requestUrl = 'https://api.rawg.io/api/games/halo-2?key=576858f4b6fa477990afac4add5f794f';
+  
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        // console.log(data);
+        console.log(data.name);
+        console.log(data.metacritic);
+        console.log(data.description);
+        console.log(data.released);
+
+
+});
+
+}     
+
+    
+    function halo3() {
+
+        var requestUrl = 'https://api.rawg.io/api/games/halo-3?key=576858f4b6fa477990afac4add5f794f';
+      
+        fetch(requestUrl)
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (data) {
+            // console.log(data);
+            console.log(data.name);
+            console.log(data.metacritic);
+            console.log(data.description);
+            console.log(data.released);
+    });
+    }     
+
+    function halo4() {
+
+        var requestUrl = 'https://api.rawg.io/api/games/halo-4?key=576858f4b6fa477990afac4add5f794f';
+      
+        fetch(requestUrl)
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (data) {
+            // console.log(data);
+            console.log(data.name);
+            console.log(data.metacritic);
+            console.log(data.description);
+            console.log(data.released);
+    });
+    }     
+
+    function halo5() {
+
+        var requestUrl = 'https://api.rawg.io/api/games/halo-5-guardians?key=576858f4b6fa477990afac4add5f794f';
+      
+        fetch(requestUrl)
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (data) {
+            // console.log(data);
+            console.log(data.name);
+            console.log(data.metacritic);
+            console.log(data.description);
+            console.log(data.released);
+    });
+    }     
+
+    function haloInfinite() {
+
+        var requestUrl = 'https://api.rawg.io/api/games/halo-infinite?key=576858f4b6fa477990afac4add5f794f';
+      
+        fetch(requestUrl)
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (data) {
+            // console.log(data);
+            console.log(data.name);
+            console.log(data.metacritic);
+            console.log(data.description);
+            console.log(data.released);
+
+          });
+        }
+
+    halo1();
+    halo2();
+    halo3();
+    halo4();
+    halo5();
+    haloInfinite();
 // function to play theme song (EASTER EGG)
 function PlaySound(soundobj) {
     var thissound=document.getElementById('mySound');
