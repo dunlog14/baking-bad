b04f5da3f950180a1103880d527ef8d4c812e0a1
carlthorne <119799879+hthcatlanta@users.noreply.github.com>
2023-02-11 21:08:16 -0500

    fixed search code

--- assets/js/youtube.js
+++ assets/js/youtube.js
@@ -43,33 +43,44 @@ const api = 'AIzaSyC3ADAar5yeEMo-GijNtQXd9rQ51fM9NGU';
 // set up http: watch string with video id to display "found" video
 //
 // What Halo games do we support with this app?
-// Halo: Combat, Halo: Evolution, Halo 2, Halo 3, Halo 4, Halo: Infinite
+// Halo: Combat, Halo: Evolution, Halo 2, Halo 3, Halo 4, Halo 5, Halo: Infinite
 //
-// Halo: Combat Evolved search string 'halo combat evolved full game'
-// Halo 2 search string 'halo 2 full campaign and cutsenses'
+// Halo: Combat Evolved search string 'halo combat evolved full game' Also referred to as Halo 1 in code.
+// Halo 2 search string 'halo 2 full campaign and cutscenes'
 // Halo 3 search string 'halo 3 pc halo: the master chief collection'
 // Halo 4 Search string 'halo 4 pc halo: the master chief collection'
 // Halo 5 Search string 'halo 5 launch gameday trailer'
 // Halo Infinite Search string 'halo infinite campaign launch trailer'
 
-let gameID = "Halo Combat Evolved";
-let searchq = "";
-console.log("gameID", gameID);
-console.log("searchq", searchq);
+// passed from HTML?
+//let gameID = "Halo Combat Evolved";
+//let gameID = "Halo 2";
+//let gameID = "Halo 3";
+//let gameID = "Halo 4";
+//let gameID = "Halo 5":
+//let gameID = "Halo Infinite";
 
-if (gameID == "Halo Combat Evolved") {
-      searchq = 'halo combat evolved full game';}
-else if (gameID == 'Halo 2') {
-      searchq = 'halo 2 full campaign and cutsenses';}
-else if (gameID == 'Halo 3') {
-      searchq = 'halo 3 pc halo: the master chief collection';}
-else if (gameID == "halo 4") {
-      searchq = 'halo 4 pc halo: the master chief collection';}
-else if (gameID == "Haloa: Infinite") {
-      searchq = 'halo infinite cmpaign launch trailer';}
-else {console.log(gameID, "Invalid Game ID");}
+//let searchq = "";
+//console.log("gameID", gameID);
+//console.log("searchq", searchq);
 
-console.log("gameID", gameID, "searchq", searchq);
+let watchyoutube = "";
+
+//if (gameID == "Halo Combat Evolved") {
+//      searchq = 'halo combat evolved full game';}
+//else if (gameID == 'Halo 2') {
+//      searchq = 'halo 2 full campaign and cutsenses';}
+//else if (gameID == 'Halo 3') {
+//      searchq = 'halo 3 pc halo: the master chief collection';}
+//else if (gameID == "halo 4") {
+ //     searchq = 'halo 4 pc halo: the master chief collection';}
+ //else if (gameID == Halo 5) {
+ //     searchq = 'halo 5 launch gameday trailer';}
+//else if (gameID == "Halo: Infinite") {
+//      searchq = 'halo infinite cmpaign launch trailer';}
+//else {console.log(gameID, "Invalid Game ID");}
+
+//console.log("gameID", gameID, "searchq", searchq);
 
 // take search item from console. comment out code once integrated with project
 
@@ -80,6 +91,7 @@ console.log("gameID", gameID, "searchq", searchq);
 
 // test url
 // copied url to web browser to test. it worked!
+searchq = 'halo combat evolved full game';
 const testurl = 'https://www.googleapis.com/youtube/v3/search/?part=snippet&key='+api+'&q='+searchq+'&maxResults=1';
 // console.log("testurl", testurl);
 
@@ -87,19 +99,167 @@ const testurl = 'https://www.googleapis.com/youtube/v3/search/?part=snippet&key=
 // https://www.googleapis.com/youtube/v3/search/?part=snippet&key=AIzaSyC3ADAar5yeEMo-GijNtQXd9rQ51fM9NGU&q='halo video game official trailer'&maxResults=1'
 // search YouTube database
 // I need to create a variable for q for each halo video game search
-function ySearch(e){
-    const url = 'https://www.googleapis.com/youtube/v3/search/?part=snippet&key='+api+'&q='+searchq+'&maxResults=1';
-    document.querySelector('.output').textContent = url;
-    console.log("url", url);
-    // part snippet, api, q=halo, max results 1
-    // q is hardcoded with game name right now
-    // q is in "" not ''
-    
-    
-    fetch(url).then(function(rep){
-    // convert json to javascript object
-    return rep.json()
-}).then(function(data){
-    console.log(data)
-})
+
+
+// Halo Combat Evolved (aka Halo 1) Search and Fetch
+searchq = 'halo combat evolved full game'
+
+function Halo1Search(){
+      const url = 'https://www.googleapis.com/youtube/v3/search/?part=snippet&key='+api+'&q='+searchq+'&maxResults=1';
+      // part snippet, api, q=halo, max results 1
+      // q is in "" not ''
+      
+      
+      fetch(url).then(function(rep){
+      // convert json to javascript object
+      return rep.json()
+  }).then(function(data){
+      console.log("youtube data", data);
+      console.log("array 0", data.items[0]);
+      console.log("youtube video ID", data.items[0].id);
+      videoId = 'lie_ysmcQV8';
+      watchyoutube = '<a href=https://www.youtube.com/watch?v='+videoId+'"target="_blank">'+video.snippet.title+'</a>';
+      console.log("youtube url", watchyoutube);
+      
+  //   show(data.items);
+  })
 }
+
+// Halo 2 Search and Fetch
+searchq = 'halo 2 full campaign and cutsenses'
+
+function Halo2Search(){
+      const url = 'https://www.googleapis.com/youtube/v3/search/?part=snippet&key='+api+'&q='+searchq+'&maxResults=1';
+      // part snippet, api, q=halo, max results 1
+      // q is in "" not ''
+      
+      
+      fetch(url).then(function(rep){
+      // convert json to javascript object
+      return rep.json()
+  }).then(function(data){
+      console.log("youtube data", data);
+      console.log("array 0", data.items[0]);
+      console.log("youtube video ID", data.items[0].id);
+      videoId = 'rl5J-rqJa3o';
+      watchyoutube = '<a href=https://www.youtube.com/watch?v='+videoId+'"target="_blank">'+video.snippet.title+'</a>';
+      console.log("youtube url", watchyoutube);
+
+  //   show(data.items);
+  })
+}
+
+// Halo 3 Search and Fetch
+searchq = 'halo 3 pc halo: the master chief collection'
+
+function Halo3Search(){
+      const url = 'https://www.googleapis.com/youtube/v3/search/?part=snippet&key='+api+'&q='+searchq+'&maxResults=1';
+      // part snippet, api, q=halo, max results 1
+      // q is in "" not ''
+      
+      
+      fetch(url).then(function(rep){
+      // convert json to javascript object
+      return rep.json()
+  }).then(function(data){
+      console.log("youtube data", data);
+      console.log("array 0", data.items[0]);
+      console.log("youtube video ID", data.items[0].id);
+      videoId = '_DtftmGZpgE';
+      watchyoutube = '<a href=https://www.youtube.com/watch?v='+videoId+'"target="_blank">'+video.snippet.title+'</a>';
+      console.log("youtube url", watchyoutube);
+
+  //   show(data.items);
+  })
+}
+
+// Halo4 Search and Fetch
+searchq = 'halo 4 pc halo: the master chief collection'
+
+function Halo4Search(){
+      const url = 'https://www.googleapis.com/youtube/v3/search/?part=snippet&key='+api+'&q='+searchq+'&maxResults=1';
+      // part snippet, api, q=halo, max results 1
+      // q is in "" not ''
+      
+      
+      fetch(url).then(function(rep){
+      // convert json to javascript object
+      return rep.json()
+  }).then(function(data){
+      console.log("youtube data", data);
+      console.log("array 0", data.items[0]);
+      console.log("youtube video ID", data.items[0].id);
+      videoId = 'YoS8i8Rx_uI';
+      watchyoutube = '<a href=https://www.youtube.com/watch?v='+videoId+'"target="_blank">'+video.snippet.title+'</a>';
+      console.log("youtube url", watchyoutube);
+
+  //   show(data.items);
+  })
+}
+
+// Halo5 Search and Fetch
+searchq = 'halo infinite cmpaign launch trailer'
+
+function Halo5Search(){
+      const url = 'https://www.googleapis.com/youtube/v3/search/?part=snippet&key='+api+'&q='+searchq+'&maxResults=1';
+      // part snippet, api, q=halo, max results 1
+      // q is in "" not ''
+      
+      
+      fetch(url).then(function(rep){
+      // convert json to javascript object
+      return rep.json()
+  }).then(function(data){
+      console.log("youtube data", data);
+      console.log("array 0", data.items[0]);
+      console.log("youtube video ID", data.items[0].id);
+      videoId = 'Rh_NXwqFvHc';
+      watchyoutube = '<a href=https://www.youtube.com/watch?v='+videoId+'"target="_blank">'+video.snippet.title+'</a>';
+      console.log("youtube url", watchyoutube);
+  //   show(data.items);
+  })
+}
+
+// Halo6 Search and Fetch
+searchq = 'halo infinite cmpaign launch trailer'
+function Halo6Search(){
+      const url = 'https://www.googleapis.com/youtube/v3/search/?part=snippet&key='+api+'&q='+searchq+'&maxResults=1';
+      // part snippet, api, q=halo, max results 1
+      // q is in "" not ''
+      
+      
+      fetch(url).then(function(rep){
+      // convert json to javascript object
+      return rep.json()
+  }).then(function(data){
+      console.log("youtube data", data);
+      console.log("array 0", data.items[0]);
+      console.log("youtube video ID", data.items[0].id);
+      videoId = 'PyMlV5_HRWk';
+      watchyoutube = '<a href=https://www.youtube.com/watch?v='+videoId+'"target="_blank">'+video.snippet.title+'</a>';
+      console.log("youtube url", watchyoutube);
+  //   show(data.items);
+  })
+}
+
+
+
+// syntax to to go youtube video with just video id
+//https://www.youtube.com/watch?v=aqz-KE-bpKQ
+
+//function show(data){
+//      console.log(data);
+//      console.log(data.length);
+//      data.forEach(function(video){
+//            console.log(video);
+//     watchyoutube = '<a href=https://www.youtube.com/watch?v='+video.id+'"target="_blank">'+video.snippet.title+'</a>';
+//     console.log("youtube url", watchyoutube);
+//      })
+//}
+
+Halo1Search();
+Halo2Search();
+Halo3Search();
+Halo4Search();
+Halo5Search();
+Halo6Search();
